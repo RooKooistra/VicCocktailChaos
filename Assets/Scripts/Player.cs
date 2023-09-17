9884f9b7ddbc66b98f68180c0b90f02b1f2b0370
Roo K <roo.kooistra@gmail.com>
2023-09-17 15:54:37 +1000

    New Input System Added and refactor controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,32 +6,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 5f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection *Time.deltaTime * moveSpeed;
